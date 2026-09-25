c20c7df4a1d2f20aaa7e20ff2ebd25a198a2ef64
ashokdpgk-wq <ashokdpgk@gmail.com>
2026-09-25 14:27:37 +0530

    Add voter data entries to voter_104_backup.js
    Added voter data entries with serial numbers, names, relations, houses, dates of birth, and EPIC numbers.

--- voter_104_backup.js
+++ voter_104_backup.js
@@ -1,1 +1,56 @@
-
+[
+  {
+    "serial": "1",
+    "name": "DIPANKAR ROY",
+    "relationType": "FATHER",
+    "relation": "CHABILAL ROY",
+    "house": "n0078",
+    "dob": "22",
+    "epic": "SKW2663193"
+  },
+  {
+    "serial": "2",
+    "name": "MANORAMA ROY",
+    "relationType": "HUSBAND",
+    "relation": "PREMANANDA ROY",
+    "house": "n0263",
+    "dob": "37",
+    "epic": "JLG3682408"
+  },
+  {
+    "serial": "3",
+    "name": "RASIDUL HOK",
+    "relationType": "FATHER",
+    "relation": "BAIDAR RAHAMAN",
+    "house": "n0317",
+    "dob": "32",
+    "epic": "SKW1602689"
+  },
+  {
+    "serial": "4",
+    "name": "ANJALI ROY",
+    "relationType": "HUSBAND",
+    "relation": "DIPENDRA NATH ROY",
+    "house": "n0037",
+    "dob": "41",
+    "epic": "JLG3237260"
+  },
+  {
+    "serial": "5",
+    "name": "RUPSANA PARVIN",
+    "relationType": "HUSBAND",
+    "relation": "RASIDUL HOK",
+    "house": "n0317",
+    "dob": "24",
+    "epic": "SKW2672004"
+  },
+  {
+    "serial": "6",
+    "name": "MANJUMA KHATUN",
+    "relationType": "HUSBAND",
+    "relation": "SARIFUL RAHAMAN",
+    "house": "n0269",
+    "dob": "40",
+    "epic": "SKW2066678"
+  }
+]
